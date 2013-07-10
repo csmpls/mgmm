4e23341dd4e2da83a37a7593ee2083a86bdd0055
Nick Merrill <yes@cosmopol.is>
2013-07-10 12:19:49 -0700

    clears screen after quit

--- talk.py
+++ talk.py
@@ -1,6 +1,6 @@
 # coding=utf-8
 import urwid
-#import os, random
+import subprocess
 import sys, urwid
 
 sys.path.insert(0,'/home/user/Documents/mgmm/modules')
@@ -14,6 +14,8 @@ from ConversationListBox import *
 script_path = '/home/user/Documents/mgmm/script.mgmm'
 diary_path = '/home/user/Documents/mgmm/diary/'
 
-palette = [('machine line', 'dark cyan,bold', 'default'),]
+palette = [('machine line', 'yellow,bold', 'default'),]
 urwid.MainLoop(ConversationListBox(script_path, diary_path), palette).run()
 
+#clear terminal screen after quit
+subprocess.Popen('clear')
