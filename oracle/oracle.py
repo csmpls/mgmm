7ad742055e3093436f8497204042a0ae0b107f4e
csmpls <csmpls@sayan-kotter.local>
2013-07-19 15:48:54 -0700

    Protoype oracle working

--- oracle/oracle.py
+++ oracle/oracle.py
@@ -1,40 +1,62 @@
 import sys, urwid
-sys.path.insert(0,'/home/user/Documents/mgmm/modules')
+sys.path.insert(0,'../modules')
 from decko import *
 
-def handle_input(key):
-   #next card on j
-   if key in ('j', 'J'):
-	shown_str = deck.next_card().get_side()
-	txt = urwid.Text(('banner', shown_str), align='center')
-        mapn = urwid.AttrMap(txt, 'streak')
-	urwid.MainLoop.map1 = mapn
-
-   #exit on q 
-	if key in ('q', 'Q'):
-		raise urwid.ExitMainLoop()
 
 
 
-def setup_display(): 
-	palette = [
-	    ('banner', 'black', 'light gray'),
-	    ('streak', 'black', 'dark red'),
-	    ('bg', 'black', 'dark blue'),]
-	
-	txt = urwid.Text(('banner', shown_str), align='center')
-	map1 = urwid.AttrMap(txt, 'streak')
-	fill = urwid.Filler(map1)
-	map2 = urwid.AttrMap(fill, 'bg')
-	loop = urwid.MainLoop(map2, palette, unhandled_input=handle_input)
-	loop.run()
-	
 
-deck = Deck('/home/user/Documents/mgmm/oracle/tarot.md')
+deck = Deck('tarot.md')
 shown_str = deck.get_card().get_side()
-setup_display()
 
 
+def exit_on_q(key):
+    if key in ('q', 'Q'):
+        raise urwid.ExitMainLoop()
+
+    if key in ('j', 'J'):
+    	shown_str = deck.next_card().get_side()
+    	txt.set_text(shown_str)
+
+    if key in ('k', 'K'):
+    	shown_str = deck.prev_card().get_side()
+    	txt.set_text(shown_str)
+    
+    if key in ('h','H'):
+    	shown_str = deck.get_card().flip_left()
+    	txt.set_text(shown_str)
+    
+    if key in ('l','L'):
+    	shown_str = deck.get_card().flip_right()
+    	txt.set_text(shown_str)
+
+palette = [
+    ('banner', '', '', '', '#ffa', '#60d'),
+    ('streak', '', '', '', 'g50', '#60a'),
+    ('inside', '', '', '', 'g38', '#808'),
+    ('outside', '', '', '', 'g27', '#a06'),
+    ('bg', '', '', '', 'g7', '#d06'),]
+
+#setup
+placeholder = urwid.SolidFill()
+loop = urwid.MainLoop(placeholder, palette, unhandled_input=exit_on_q)
+loop.screen.set_terminal_properties(colors=256)
+loop.widget = urwid.AttrMap(placeholder, 'bg')
+loop.widget.original_widget = urwid.Filler(urwid.Pile([]))
+
+# setup the attributes
+div = urwid.Divider()
+outside = urwid.AttrMap(div, 'outside')
+inside = urwid.AttrMap(div, 'inside')
+txt = urwid.Text(('banner', shown_str), align='center')
+streak = urwid.AttrMap(txt, 'streak')
+
+# add the attributes to a pile
+pile = loop.widget.base_widget # .base_widget skips the decorations
+for item in [outside, inside, streak, inside, outside]:
+    pile.contents.append((item, pile.options()))
+
+loop.run()
 
 
 
